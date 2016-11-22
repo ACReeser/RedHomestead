d59d87c68bba82931695951e5fe600ae5b6026bb
Alex Reeser <alexcreeser@gmail.com>
2016-11-22 13:30:22 -0600

    New animated pipe

---
+++ Assets/homestead/base pieces/buildable modules/AnimatedTexture.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimatedTexture : MonoBehaviour {
+    public float speed = 1f;
+    public Vector2 direction = Vector2.right;
+    public bool animateDiffuse = true, animateEmissive = false;
+    public string diffuseName = "_MainTex", emissiveName = "_EmissionMap";
+
+    private Material animatedMaterial;
+
+    
+	// Use this for initialization
+	void Start () {
+        this.animatedMaterial = this.GetComponent<Renderer>().material;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        CheckMoveTexture(animateDiffuse, diffuseName);
+
+        CheckMoveTexture(animateEmissive, emissiveName);
+    }
+
+    private void CheckMoveTexture(bool doAnimate, string textureName)
+    {
+        if (doAnimate)
+            this.animatedMaterial.SetTextureOffset(textureName, this.animatedMaterial.GetTextureOffset(textureName) + (direction * speed * Time.deltaTime));
+    }
+}
