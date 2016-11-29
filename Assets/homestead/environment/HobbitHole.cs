67e1d108981c993dbd11cd8b616159a9cb144eaa
ACReeser <rogueconformist@gmail.com>
2016-11-29 08:19:28 -0600

    Cavern code first draft complete
    needs a lot of validation checks yet

--- Assets/homestead/environment/HobbitHole.cs
+++ Assets/homestead/environment/HobbitHole.cs
@@ -8,25 +8,36 @@ public class HobbitHole : MonoBehaviour {
     private const int YDiameter = 2;
     private const int ZDepth = 10;
 
-    private class CavernMapClass
+    private class OffsetArray<T>
     {
-        public Transform[,,] Transforms;
-        public bool[,,] IsExcavated;
+        private int XOffset = 5;
+        private int YOffset = 2;
+        private int ZOffset = 0;
 
-        public Transform this[Vector3 localPosition]
+        public T[,,] Array;
+
+        public OffsetArray(int xSize, int ySize, int zSize)
+        {
+            Array = new T[xSize, ySize, zSize];
+        }
+
+        public T this[Vector3 localPosition]
         {
             get
             {
-                return this.Transforms[(int)localPosition.x + XDiameter, (int)localPosition.y + YDiameter, (int)localPosition.z];
+                return this.Array[(int)localPosition.x + XOffset, (int)localPosition.y + YOffset, (int)localPosition.z + ZOffset];
+            }
+            set
+            {
+                this.Array[(int)localPosition.x + XOffset, (int)localPosition.y + YOffset, (int)localPosition.z + ZOffset] = value;
             }
         }
-
     }
 
-    public Transform cavernWallPrefab, cavernPrefab;
+    public Transform cavernWallPrefab, cavernPrefab, originWall;
 
-    private Transform[,,] CavernTransforms = new Transform[(2 * XDiameter) + 1, (2 * YDiameter) + 1, ZDepth];
-    private bool[,,] CavernMap = new bool[(2 * XDiameter) + 1, (2 * YDiameter) + 1, ZDepth];
+    private OffsetArray<Transform> CavernTransforms = new OffsetArray<Transform>((2 * XDiameter) + 1, (2 * YDiameter) + 1, ZDepth);
+    private OffsetArray<bool> CavernMap = new OffsetArray<bool>((2 * XDiameter) + 1, (2 * YDiameter) + 1, ZDepth);
     
     private TransformBuffer CavernWallBuffer = new TransformBuffer();
     private TransformBuffer CavernBuffer = new TransformBuffer();
@@ -41,6 +52,8 @@ public class HobbitHole : MonoBehaviour {
         {
             Transform newT = GameObject.Instantiate<Transform>(Prefab);
             newT.parent = this.Parent;
+            newT.localScale = Vector3.one;
+            newT.localRotation = Quaternion.identity;
             AddExisting(newT);
         }
 
@@ -89,6 +102,8 @@ public class HobbitHole : MonoBehaviour {
             CavernWallBuffer.AddNew();
             CavernBuffer.AddNew();
         }
+
+        CavernTransforms[Vector3.zero] = originWall;
     }
 
     // Update is called once per frame
@@ -98,15 +113,15 @@ public class HobbitHole : MonoBehaviour {
 
     public void Excavate(Vector3 lPosition)
     {
-        if (!CavernMap[(int)lPosition.x, (int)lPosition.y, (int)lPosition.z])
+        if (!CavernMap[lPosition])
         {
-            Transform existingCavernWall = CavernTransforms[(int)lPosition.x, (int)lPosition.y, (int)lPosition.z];
+            Transform existingCavernWall = CavernTransforms[lPosition];
 
             CavernWallBuffer.AddExisting(existingCavernWall);
 
-            CavernTransforms[(int)lPosition.x, (int)lPosition.y, (int)lPosition.z] = CavernBuffer.Get(lPosition);
+            CavernTransforms[lPosition] = CavernBuffer.Get(lPosition);
 
-            CavernMap[(int)lPosition.x, (int)lPosition.y, (int)lPosition.z] = true;
+            CavernMap[lPosition] = true;
 
             FillAdjacentTransformless(lPosition);
         }
@@ -117,20 +132,23 @@ public class HobbitHole : MonoBehaviour {
         Fill(fromLocalPosition + Vector3.left, true);
         Fill(fromLocalPosition + Vector3.right, true);
         Fill(fromLocalPosition + Vector3.forward, true);
-        Fill(fromLocalPosition + Vector3.back, true);
+
+        if (fromLocalPosition.z > 0)
+            Fill(fromLocalPosition + Vector3.back, true);
+
         Fill(fromLocalPosition + Vector3.up, true);
         Fill(fromLocalPosition + Vector3.down, true);
     }
 
     public void Fill(Vector3 lPosition, bool onlyVoidCells = false)
     {
-        Transform existingTransform = CavernTransforms[(int)lPosition.x, (int)lPosition.y, (int)lPosition.z];
+        Transform existingTransform = CavernTransforms[lPosition];
         bool isVoidCell = existingTransform == null;
 
         if (onlyVoidCells && !isVoidCell)
             return;
 
-        bool isEmpty = CavernMap[(int)lPosition.x, (int)lPosition.y, (int)lPosition.z];
+        bool isEmpty = CavernMap[lPosition];
         
         if (isEmpty)
         {
@@ -140,8 +158,8 @@ public class HobbitHole : MonoBehaviour {
             }
         }
 
-        CavernTransforms[(int)lPosition.x, (int)lPosition.y, (int)lPosition.z] = CavernWallBuffer.Get(lPosition);
+        CavernTransforms[lPosition] = CavernWallBuffer.Get(lPosition);
 
-        CavernMap[(int)lPosition.x, (int)lPosition.y, (int)lPosition.z] = false;
+        CavernMap[lPosition] = false;
     }
 }
