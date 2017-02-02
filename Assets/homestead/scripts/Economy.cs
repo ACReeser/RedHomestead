db1a8578ae58c12bc03fc86a176ef5155b87b089
ACReeser <rogueconformist@gmail.com>
2017-02-01 22:52:39 -0600

    Starting on economy scripting
    trying to knock out budget tab real quick

---
+++ Assets/homestead/scripts/Economy.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Economy : MonoBehaviour {
+    public delegate void EconomyHandler();
+
+    public static Economy Instance;
+
+    public event EconomyHandler OnBankAccountChange;
+
+    public int HoursUntilPayday;
+    public int PaydayAmount;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(EconomyTick());
+	}
+
+    private IEnumerator EconomyTick()
+    {
+        while (isActiveAndEnabled)
+        {
+            yield return null;
+        }
+    }
+}
