6a58fd225165ddc87acacb3aa52bb45980459586
ACReeser <rogueconformist@gmail.com>
2017-02-02 23:09:37 -0600

    Market tabs implemented

--- Assets/homestead/gui/Terminal.cs
+++ Assets/homestead/gui/Terminal.cs
@@ -1,25 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public enum TerminalProgram { Finances, Colony, News, Market}
+public enum TerminalProgram { Finances, Colony, News, Market }
+public enum MarketTab { Buy, EnRoute, Sell }
+public enum BuyTab { ByResource, BySupplier, Checkout }
+
 public class Terminal : MonoBehaviour {
 
-    public RectTransform[] ProgramPanels;
+    public RectTransform[] ProgramPanels, MarketTabs, BuyTabs;
     public RectTransform HomePanel;
 
-    private RectTransform currentProgramPanel;
+    private RectTransform currentProgramPanel, currentMarketTab, currentBuyTab;
 
 	// Use this for initialization
-	void Start () {
-        foreach(RectTransform t in ProgramPanels)
+	void Start ()
+    {
+        HideAll(ProgramPanels);
+        HideAll(MarketTabs);
+        HideAll(BuyTabs);
+
+        SetProgram(null);
+    }
+
+    private void HideAll(RectTransform[] collection)
+    {
+        foreach (RectTransform t in collection)
         {
             t.gameObject.SetActive(false);
         }
-        SetProgram(null);
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
 
@@ -42,7 +54,34 @@ public class Terminal : MonoBehaviour {
 
         currentProgramPanel = panel;
 
+        if (currentProgramPanel == ProgramPanels[(int)TerminalProgram.Market])
+            SwitchMarketTab((int)MarketTab.Buy);
+
         if (currentProgramPanel != null)
             currentProgramPanel.gameObject.SetActive(true);
+    }
+
+    public void SwitchMarketTab(int t)
+    {
+
+        if (currentMarketTab != null)
+            currentMarketTab.gameObject.SetActive(false);
+
+        currentMarketTab = MarketTabs[t];
+
+        if (currentMarketTab == MarketTabs[(int)MarketTab.Sell])
+            SwitchBuyTab((int)BuyTab.BySupplier);
+
+        currentMarketTab.gameObject.SetActive(true);
+    }
+
+    public void SwitchBuyTab(int t)
+    {
+        if (currentBuyTab != null)
+            currentBuyTab.gameObject.SetActive(false);
+
+        currentBuyTab = BuyTabs[t];
+
+        currentBuyTab.gameObject.SetActive(true);
     }
 }
