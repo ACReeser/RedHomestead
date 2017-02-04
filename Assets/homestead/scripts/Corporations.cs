19eec91897e92f111a1bfd09c737eeb618bacac1
ACReeser <rogueconformist@gmail.com>
2017-02-03 23:47:42 -0600

    New corps file

---
+++ Assets/homestead/scripts/Corporations.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using RedHomestead.Economy;
+using RedHomestead.Geography;
+
+public static class Corporations{
+
+    public static List<Vendor> Wholesalers = new List<Vendor>()
+    {
+        new Vendor()
+        {
+            Name = "MonsMart",
+            AvailableDelivery = DeliveryType.Drop & DeliveryType.Lander & DeliveryType.Rover,
+            Location = new LatLong()
+            {
+                LatitudeDegrees = 18.65f,
+                LongitudeDegrees = 226.2f
+            },
+            Stock = new List<Stock>()
+            {
+
+            }
+        },
+        new Vendor()
+        {
+            Name = "Bradbury & Co.",
+            AvailableDelivery = DeliveryType.Drop & DeliveryType.Lander & DeliveryType.Rover,
+            Location = new LatLong()
+            {
+                LatitudeDegrees = -4.59f,
+                LongitudeDegrees = 137.44f
+            },
+            Stock = new List<Stock>()
+            {
+
+            }
+        }
+    };
+}
