cfc24a269b1f511f82a7d23387dd85a5786883e1
ACReeser <rogueconformist@gmail.com>
2017-03-03 14:12:40 -0600

    Archetypes AI file, starting persistence scripting

---
+++ Assets/homestead/scripts/Commander.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using RedHomestead.Persistence;
+using System;
+
+//namespace RedHomestead.Commands
+//{
+//    public enum CommandType { AddCrate }
+
+//    public struct Command
+//    {
+
+//    }
+//}
+
+public class Commander : MonoBehaviour {
+    public static Commander Instance;
+    public bool Autosaving = false;
+
+    private Base currentBase;
+    private float AutsaveSeconds = 2 * 60;
+
+    void Awake () {
+        Instance = this;
+        DontDestroyOnLoad(this.gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Autosave());
+	}
+
+    private IEnumerator Autosave()
+    {
+        while(isActiveAndEnabled && Autosaving)
+        {
+            yield return new WaitForSeconds(AutsaveSeconds);
+            Save();
+        }
+    }
+
+    //public void Do()
+    //{
+
+    //}
+
+    public void Save()
+    {
+        PersistentDataManager.SaveBase(currentBase);
+    }
+}
---
+++ Assets/homestead/scripts/Persistence.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RedHomestead.Persistence
+{
+    [Serializable]
+    public class Base {
+        //player data
+        public List<ResourceComponent> Crates { get; set; }
+        public List<Habitat> Habitats { get; set; }
+        //hobbit hole data
+        //floorplan data
+        //stuff data
+        //module data
+        ////container data
+        ////pipe data
+    }
+
+    public static class PersistentDataManager
+    {
+        public const string baseFileName = "base.dat";
+        public static BinaryFormatter _formatter = new BinaryFormatter();
+
+        public static void SaveBase(Base toSave)
+        {
+            using(FileStream file = File.Open(Path.Combine(UnityEngine.Application.persistentDataPath, baseFileName), FileMode.OpenOrCreate))
+            {
+                _formatter.Serialize(file, toSave);
+            }
+        }
+
+        public static Base LoadBase()
+        {
+            using (FileStream file = File.Open(Path.Combine(UnityEngine.Application.persistentDataPath, baseFileName), FileMode.Open))
+            {
+                return _formatter.Deserialize(file) as Base;
+            }
+        }
+    }
+
+}
