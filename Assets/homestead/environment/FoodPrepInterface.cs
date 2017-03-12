f8d12e6f54eea15abb6b1488361624fedaf8dc7b
ACReeser <rogueconformist@gmail.com>
2017-03-12 10:00:15 -0500

    Food prep scripting

--- Assets/homestead/environment/FoodPrepInterface.cs
+++ Assets/homestead/environment/FoodPrepInterface.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using RedHomestead.Simulation;
 
 public class FoodPrepInterface : HabitatModule
 {
@@ -8,6 +9,29 @@ public class FoodPrepInterface : HabitatModule
 
     protected override void OnStart()
     {
+        if (this.LinkedHab != null)
+        {
+            this.LinkedHab.OnResourceChange += this.OnResourceChange;
+            this.OnResourceChange();
+        }
+    }
 
+    private void OnResourceChange(params Matter[] type)
+    {
+        ResourceContainer container = this.LinkedHab.Get(Matter.Biomass);
+
+        float meals = container.CurrentAmount * container.MatterType.MealsPerCubicMeter();
+
+        for (int i = 0; i < BiomassStorageRoot.childCount; i++)
+        {
+            BiomassStorageRoot.GetChild(i).gameObject.SetActive(meals >= i + 1);
+        }
+
+        BiomassVisualization.gameObject.SetActive(meals > 0f);
+
+        container = this.LinkedHab.Get(Matter.MealPowder);
+
+        float powders = container.CurrentAmount * container.MatterType.MealsPerCubicMeter();
+        this.PowderVisualization.gameObject.SetActive(powders > 0f);
     }
 }
