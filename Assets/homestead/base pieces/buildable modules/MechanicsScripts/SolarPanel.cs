c474fbb3dd58ba3aca91e467fee8f744657c01e5
ACReeser <rogueconformist@gmail.com>
2017-04-09 22:23:08 -0500

    New Solar panel mesh and skin, with sun tracking

--- Assets/homestead/base pieces/buildable modules/MechanicsScripts/SolarPanel.cs
+++ Assets/homestead/base pieces/buildable modules/MechanicsScripts/SolarPanel.cs
@@ -4,6 +4,7 @@ using System;
 using RedHomestead.Simulation;
 using RedHomestead.Buildings;
 using RedHomestead.Electricity;
+using RedHomestead.Persistence;
 
 public class SolarPanel : ResourcelessGameplay, IPowerSupply
 {
@@ -11,6 +12,7 @@ public class SolarPanel : ResourcelessGameplay, IPowerSupply
     public float GrossSolarWattagePerTick = 20f;
     public MeshFilter powerBacking { get; set; }
     public Transform powerMask { get; set; }
+    public Transform[] PivotPoles = new Transform[2];
 
     public override float WattsConsumed
     {
@@ -31,7 +33,37 @@ public class SolarPanel : ResourcelessGameplay, IPowerSupply
 
     public override void OnAdjacentChanged()
     {
-        
+    }
+
+    private const float TiltAmount = 50f;
+    private void _OnHourChange(int sol, float hour)
+    {
+        if (hour < 19 && hour > 5)
+        {
+            float tiltAmount = Mathf.Lerp(90f + TiltAmount, 90f - TiltAmount, (float)(hour - 6f) / 12f);
+            StartCoroutine(RotateTo(tiltAmount));
+        }
+    }
+
+    private IEnumerator RotateTo(float tiltAmount)
+    {
+        Quaternion from = PivotPoles[0].localRotation;
+        Quaternion to = Quaternion.Euler(0f, -90f, tiltAmount);
+
+        float time = 0f;
+        while(time < 1f)
+        {
+            foreach(Transform panel in PivotPoles)
+            {
+                panel.localRotation = Quaternion.Lerp(from, to, time);
+            }            
+            time += Time.deltaTime;
+            yield return null;
+        }
+        foreach (Transform panel in PivotPoles)
+        {
+            panel.localRotation = to;
+        }
     }
 
     public override void Tick()
@@ -56,5 +88,11 @@ public class SolarPanel : ResourcelessGameplay, IPowerSupply
     public override Module GetModuleType()
     {
         return Module.SolarPanelSmall;
+    }
+
+    protected override void OnStart()
+    {
+        SunOrbit.Instance.OnHourChange += _OnHourChange;
+        _OnHourChange(Game.Current.Environment.CurrentSol, Game.Current.Environment.CurrentHour);
     }
 }
