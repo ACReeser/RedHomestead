d0040dfa88781082d75676fb082b1ebdb440faa0
ACReeser <rogueconformist@gmail.com>
2017-04-28 14:02:44 -0500

    Gremlin code to spawn electrical and pressure failures
    Will be particle systems, of varying types

--- Assets/homestead/scripts/Gremlin.cs
+++ Assets/homestead/scripts/Gremlin.cs
@@ -6,7 +6,16 @@ using UnityEngine;
 
 public interface IRepairable
 {
+    float RepairProgress { get; set; }
+}
 
+public interface IElectricalRepairable: IRepairable
+{
+    Transform ElectricalFailureAnchor { get; }
+}
+public interface IPressureRepairable : IRepairable
+{
+    Transform PressureFailureAnchor { get; }
 }
 
 public class Gremlin : MonoBehaviour {
@@ -20,10 +29,16 @@ public class Gremlin : MonoBehaviour {
     private const int WealthyPlayerThreshold = 200000;
     private const float PenaltyPerRepairable = .333f;
     private const int GremlinMissesThreshold = 4;
-    public Transform ElectricalFailureSparksPrefab;
+    public Transform ElectricalFailureSparksPrefab, OutgassingFailurePrefab;
+    internal Gremlin Instance { get; private set; }
 
-	// Use this for initialization
-	void Start () {
+    void Awake()
+    {
+        Instance = this;
+    }
+    
+	void Start()
+    {
         StartCoroutine(Lurk());
 	}
 
@@ -39,12 +54,10 @@ public class Gremlin : MonoBehaviour {
 
     private void Plot()
     {
-        List<IRepairable> repairables = GetAllRegisteredRepairables();
-
-        if (repairables.Count > 0)
+        if (registeredRepairables.Count > 0)
         {
             int roll = UnityEngine.Random.Range(1, 21);
-            int dc = GetDifficultyCheck(repairables);
+            int dc = GetDifficultyCheck(registeredRepairables);
 
             if (roll > dc)
             {
@@ -96,16 +109,87 @@ public class Gremlin : MonoBehaviour {
         return Mathf.RoundToInt(dc);
     }
 
+    //todo: use map to struct with both part system transform and failType
+    //and use that as currentlygremlind, with keys.count > 0 as "has victim"
+    private Dictionary<IRepairable, Transform> repairableToParticleSystemMap = new Dictionary<IRepairable, Transform>();
     private List<IRepairable> currentlyGremlind = new List<IRepairable>();
     private IEnumerator CauseHavok()
     {
-        yield return new WaitForSeconds(1f);
+        IRepairable victim = registeredRepairables[UnityEngine.Random.Range(0, registeredRepairables.Count)];
+
+        if (victim is IElectricalRepairable)
+        {
+            Transform particleSystem = GetParticleSystemPrefab(FailureType.Electrical);
+            particleSystem.SetParent((victim as IElectricalRepairable).ElectricalFailureAnchor);
+            particleSystem.transform.localPosition = Vector3.zero;
+            particleSystem.transform.localRotation = Quaternion.identity;
+            repairableToParticleSystemMap.Add(victim, particleSystem);
+        }
+
+        currentlyGremlind.Add(victim);
+
+        bool hasGremlinedRepairable = true;
+
+        while (hasGremlinedRepairable)
+        {
+
+            yield return new WaitForSeconds(1f);
+
+            hasGremlinedRepairable = currentlyGremlind.Count > 0;
+        }
+
+        StartCoroutine(Lurk());
     }
 
     private List<IRepairable> registeredRepairables = new List<IRepairable>();
-
-    private List<IRepairable> GetAllRegisteredRepairables()
+    internal void Register(IRepairable repairable)
     {
-        return registeredRepairables;
+        registeredRepairables.Add(repairable);
+    }
+
+    internal void Deregister(IRepairable repairable)
+    {
+        registeredRepairables.Remove(repairable);
+    }
+
+    internal void FinishRepair(IRepairable repairable)
+    {
+        currentlyGremlind.Remove(repairable);
+        //todo: add particle system back to pool
+        //repairableToParticleSystemMap[repairable]
+    }
+
+    public enum FailureType { Electrical, Pressure }
+    private Dictionary<FailureType, List<Transform>> particleSystemPool = new Dictionary<FailureType, List<Transform>>()
+    {
+        { FailureType.Electrical, new List<Transform>() },
+        { FailureType.Pressure, new List<Transform>() }
+    };
+
+    private Transform GetParticleSystemPrefab(FailureType failType)
+    {
+        switch (failType)
+        {
+            case FailureType.Pressure:
+                return OutgassingFailurePrefab;
+            default:
+                return ElectricalFailureSparksPrefab;
+        }
+    }
+
+    internal Transform GetFailureParticleSystemFromPool(FailureType failType)
+    {
+        List<Transform> pool = particleSystemPool[failType];
+
+        if (pool.Count > 0)
+        {
+            Transform result = pool[0];
+            pool.RemoveAt(0);
+            return result;
+        }
+        else
+        {
+            return (Transform)GameObject.Instantiate(GetParticleSystemPrefab(failType));
+        }
     }
 }
