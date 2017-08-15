08620f2655c7d1ae6556abdabacc828b115e8c40
ACReeser <rogueconformist@gmail.com>
2017-08-14 21:58:55 -0500

    Toolbox script start

---
+++ Assets/homestead/prefabs/craftables/Toolbox.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Toolbox : MonoBehaviour, IDoorManager
+{
+    private DoorRotationLerpContext lerp;
+    private bool open = false;
+    
+    public void ToggleDoor(Transform door)
+    {
+        //assumes all door transforms start shut
+        if (lerp == null)
+            lerp = new DoorRotationLerpContext(door, door.localRotation, Quaternion.Euler(0, -90, 90f), .4f);
+
+        lerp.Toggle(StartCoroutine);
+    }
+}
