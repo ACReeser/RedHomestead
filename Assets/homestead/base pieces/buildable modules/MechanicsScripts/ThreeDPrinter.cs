629699300d1a52ca95b3717d295b2bee48cc9747
ACReeser <rogueconformist@gmail.com>
2017-08-19 20:24:17 -0500

    3D printer mesh + prefab + door open

---
+++ Assets/homestead/base pieces/buildable modules/MechanicsScripts/ThreeDPrinter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThreeDPrinter : MonoBehaviour, IDoorManager {
+    private DoorRotationLerpContext lerp;
+
+    public void ToggleDoor(Transform door)
+    {
+        //assumes all door transforms start shut
+        if (lerp == null)
+            lerp = new DoorRotationLerpContext(door, door.localRotation, Quaternion.Euler(-60, 0, -90f), .8f);
+        
+        lerp.Toggle(StartCoroutine);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
