a18b807a77cfab6c7214bb5b86a3df4fa6e8b61c
ACReeser <rogueconformist@gmail.com>
2017-10-18 22:56:30 -0500

    Tutorial modals with input
    and lander in tutorial

--- Assets/homestead/scripts/Tutorial.cs
+++ Assets/homestead/scripts/Tutorial.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RedHomestead.Simulation;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
@@ -7,12 +8,36 @@ using UnityEngine.UI;
 
 public enum FinishTutorialChoice { NextLesson, ExitToMenu, ListOfLessons }
 
+[Serializable]
+public class EventPanel
+{
+    public CanvasGroup Group;
+    public Text Header, Description;
+    public Image Sprite;
+
+    internal bool Visible;
+
+    public void Fill(string header, string description, Sprite sprite)
+    {
+        Header.text = header;
+        Description.text = description;
+        Sprite.sprite = sprite;
+    }
+}
+
+[Serializable]
+public class MainTutorialPanel
+{
+    public RectTransform Panel, ContinuePanel;
+    public Text Header, Description, Steps;
+}
+
 public class Tutorial : MonoBehaviour, ITriggerSubscriber
 {
     public Canvas TutorialCanvas;
-    public RectTransform TutorialPanel;
-    public Text Header, Description;
+    public EventPanel EventPanel;
     public Image Backdrop;
+    public MainTutorialPanel TutorialPanel;
 
     public TriggerForwarder Mars101_LZTarget;
 
@@ -32,7 +57,8 @@ public class Tutorial : MonoBehaviour, ITriggerSubscriber
         };
         Backdrop.enabled = true;
         Backdrop.canvasRenderer.SetAlpha(0f);
-        TutorialPanel.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, GetLeftScreenHugXInset(), TutorialPanel.sizeDelta.x);
+        EventPanel.Group.gameObject.SetActive(false);
+        TutorialPanel.Panel.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, GetLeftScreenHugXInset(), TutorialPanel.Panel.sizeDelta.x);
         CurrentLesson.Start();
 	}
 
@@ -52,13 +78,11 @@ public class Tutorial : MonoBehaviour, ITriggerSubscriber
             CurrentLesson.Start();
         }
     }
-	
-	// Update is called once per frame
+    
 	void Update () {
-		
 	}
 
-    public void ResetCurrentFlags() { currentForwarder = null;  currentlyColliding = null;  currentResource = null; }
+    public void ResetTriggerColliderFlags() { currentForwarder = null;  currentlyColliding = null;  currentResource = null; }
     public TriggerForwarder currentForwarder{ get; private set;}
     public Collider currentlyColliding{ get; private set;}
     public IMovableSnappable currentResource{ get; private set;}
@@ -80,7 +104,7 @@ public class Tutorial : MonoBehaviour, ITriggerSubscriber
 
     internal float GetCenterScreenXInset()
     {
-        return (TutorialCanvas.pixelRect.width / 2f) - (TutorialPanel.sizeDelta.x / 2f);
+        return (TutorialCanvas.pixelRect.width / 2f) - (TutorialPanel.Panel.sizeDelta.x / 2f);
     }
 }
 
@@ -114,18 +138,42 @@ public abstract class TutorialLesson
         this.Description = GetDescription();
     }
 
+    public int currentStep { get; private set; }
+    protected void CompleteCurrentStep()
+    {
+        Description.StepsComplete[currentStep] = true;
+        currentStep++;
+        UpdateStepsText();
+    }
+
     protected void UpdateDescription()
     {
-        self.Header.text = Description.Name;
-        StringBuilder sb = new StringBuilder(Description.Description);
-        sb.AppendLine(); sb.AppendLine();
+        self.TutorialPanel.Header.text = Description.Name;
+        self.TutorialPanel.Description.text = Description.Description;
+        self.TutorialPanel.ContinuePanel.gameObject.SetActive(true);
+        self.TutorialPanel.Description.gameObject.SetActive(true);
+        self.TutorialPanel.Steps.gameObject.SetActive(false);
+    }
+    protected void UpdateStepsText()
+    {
+        StringBuilder sb = new StringBuilder();
+        //sb.AppendLine(); sb.AppendLine();
         for (int i = 0; i < Description.Steps.Length; i++)
         {
+            if (i > currentStep)
+                break;
+
             sb.Append(Description.StepsComplete[i] ? "✔ " : "* ");
             sb.Append(Description.Steps[i]);
+            if (i < Description.Steps.Length - 1)
+                sb.Append('\n');
         }
-        self.Description.text = sb.ToString();
+        self.TutorialPanel.Steps.text = sb.ToString();
+        self.TutorialPanel.ContinuePanel.gameObject.SetActive(false);
+        self.TutorialPanel.Description.gameObject.SetActive(false);
+        self.TutorialPanel.Steps.gameObject.SetActive(true);
     }
+
     internal abstract TutorialDescription GetDescription();
 
     public void Start()
@@ -136,6 +184,7 @@ public abstract class TutorialLesson
     protected IEnumerator ToggleTutorialPanel()
     {
         float newBackdropAlpha;
+        bool newBackdropActive;
         bool moveCenterToLeft = self.TutorialPanelInForeground;
         bool moveLeftToCenter = !self.TutorialPanelInForeground;
         
@@ -145,27 +194,57 @@ public abstract class TutorialLesson
             startInset = self.GetCenterScreenXInset();
             endInset = self.GetLeftScreenHugXInset();
             newBackdropAlpha = 0f;
+            newBackdropActive = false;
         }
         else //move left to center
         {
             startInset = self.GetLeftScreenHugXInset();
             endInset = self.GetCenterScreenXInset();
             newBackdropAlpha = 221f / 256f;
+            newBackdropActive = true;
         }
         float time = 0f; float duration = .6f;
+        self.Backdrop.gameObject.SetActive(!newBackdropActive);
         self.Backdrop.CrossFadeAlpha(newBackdropAlpha, duration, true);
 
-        self.TutorialPanel.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, startInset, self.TutorialPanel.sizeDelta.x);
+        self.TutorialPanel.Panel.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, startInset, self.TutorialPanel.Panel.sizeDelta.x);
 
         while (time < duration)
         {
-            self.TutorialPanel.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, Mathf.Lerp(startInset, endInset, time / duration), self.TutorialPanel.sizeDelta.x);
+            self.TutorialPanel.Panel.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, Mathf.Lerp(startInset, endInset, time / duration), self.TutorialPanel.Panel.sizeDelta.x);
             yield return null;
             time += Time.deltaTime;
         }
 
-        self.TutorialPanel.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, endInset, self.TutorialPanel.sizeDelta.x);
+        self.Backdrop.gameObject.SetActive(newBackdropActive);
+        self.TutorialPanel.Panel.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, endInset, self.TutorialPanel.Panel.sizeDelta.x);
         self.TutorialPanelInForeground = !self.TutorialPanelInForeground;
+    }
+
+    
+    protected IEnumerator ToggleEventPanel()
+    {
+        float from = (self.EventPanel.Visible) ? 1f : 0f;
+        float to = (self.EventPanel.Visible) ? 0f : 1f;
+
+        if (!self.EventPanel.Visible)
+            self.EventPanel.Group.gameObject.SetActive(true);
+
+        float time = 0f; float duration = .6f;
+
+        self.EventPanel.Group.alpha = from;
+        while (time < duration)
+        {
+            self.EventPanel.Group.alpha = Mathf.Lerp(from, to, time / duration);
+            yield return null;
+            time += Time.deltaTime;
+        }
+        self.EventPanel.Group.alpha = to;
+
+        self.EventPanel.Visible = !self.EventPanel.Visible;
+
+        if (!self.EventPanel.Visible)
+            self.EventPanel.Group.gameObject.SetActive(false);
     }
 
     protected void End()
@@ -181,13 +260,17 @@ public class MarsSurvival101: TutorialLesson
 {
     public MarsSurvival101(Tutorial self, Func<IEnumerator, Coroutine> startCo): base(self, startCo) { }
 
+    protected bool ContinueButtonDown()
+    {
+        return Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter) || Input.GetKeyDown(KeyCode.Space);
+    }
+
     protected override IEnumerator Main()
     {
         yield return this.ToggleTutorialPanel();
-
-        yield return new WaitForSeconds(5f);
-
+        yield return new WaitUntil(ContinueButtonDown);
         yield return this.ToggleTutorialPanel();
+        UpdateStepsText();
 
         self.Mars101_LZTarget.transform.position = self.LZ.transform.position;
         self.Mars101_LZTarget.gameObject.SetActive(true);
@@ -198,9 +281,32 @@ public class MarsSurvival101: TutorialLesson
         }
         while (!self.PlayerInLZ);
 
-        self.ResetCurrentFlags();
+        self.ResetTriggerColliderFlags();
         self.Mars101_LZTarget.gameObject.SetActive(false);
-        Description.StepsComplete[0] = true;
+        CompleteCurrentStep();
+
+        yield return ToggleEventPanel();
+        yield return new WaitUntil(ContinueButtonDown);
+        yield return ToggleEventPanel();
+
+        self.LZ.Deliver(new RedHomestead.Economy.Order()
+        {
+            LineItemUnits = new ResourceUnitCountDictionary()
+            {
+                { Matter.Piping, 2 },
+                { Matter.IronSheeting, 2 },
+            },
+            Vendor = new RedHomestead.Economy.Vendor(),
+            Via = RedHomestead.Economy.DeliveryType.Lander,
+        });
+
+        do
+        {
+            yield return new WaitForSeconds(1f);
+        }
+        while (self.LZ.Cargo == null || self.LZ.Cargo.Data.State == CargoLander.FlightState.Landing);
+
+        CompleteCurrentStep();
 
         End();
     }
@@ -214,7 +320,8 @@ public class MarsSurvival101: TutorialLesson
 In order to prepare new homesteaders for the harsh Martian terrain, the <b>UN MARS AUTHORITY</b> has set up this training base.",
             Steps = new string[]
             {
-                "Please walk using <b>WASD</b> to the circle of lights: the <b>LANDING ZONE</b>."
+                "Walk using <b>WASD</b> to the circle of lights: the <b>LANDING ZONE</b>.",
+                "Step away from the <b>LANDING ZONE</b> and wait for the <b>CARGO LANDER</b>."
             }
         };
     }
