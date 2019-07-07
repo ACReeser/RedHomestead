ed8140cb175b0013ca18c81fe3e3ddaa8cac067b
ACReeser <rogueconformist@gmail.com>
2019-07-07 13:53:24 -0500

    Walking colonist

---
+++ Assets/homestead/colonists/HomesteaderController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Animator))]
+public class HomesteaderController : MonoBehaviour
+{
+    Animator anim;
+    NavMeshAgent agent;
+    Vector2 smoothDeltaPosition = Vector2.zero;
+    Vector2 velocity = Vector2.zero;
+
+    public Transform WalkToModuleTarget;
+
+    // Use this for initialization
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        // Don’t update position automatically
+        agent.updatePosition = false;
+
+        if (WalkToModuleTarget != null)
+            agent.destination = WalkToModuleTarget.position;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 worldDeltaPosition = agent.nextPosition - transform.position;
+
+        // Map 'worldDeltaPosition' to local space
+        float dx = Vector3.Dot(transform.right, worldDeltaPosition);
+        float dy = Vector3.Dot(transform.forward, worldDeltaPosition);
+        Vector2 deltaPosition = new Vector2(dx, dy);
+
+        // Low-pass filter the deltaMove
+        float smooth = Mathf.Min(1.0f, Time.deltaTime / 0.15f);
+        smoothDeltaPosition = Vector2.Lerp(smoothDeltaPosition, deltaPosition, smooth);
+
+        // Update velocity if time advances
+        if (Time.deltaTime > 1e-5f)
+            velocity = smoothDeltaPosition / Time.deltaTime;
+
+        bool shouldMove = velocity.magnitude > 0.5f && agent.remainingDistance > agent.radius;
+
+        // Update animation parameters
+        anim.SetBool("Moving", shouldMove);
+        anim.SetFloat("VelocityX", velocity.x);
+        anim.SetFloat("VelocityY", velocity.y);
+
+        //GetComponent<LookAt>().lookAtTargetPosition = agent.steeringTarget + transform.forward;
+    }
+    
+    void OnAnimatorMove()
+    {
+        // Update position to agent position
+        transform.position = agent.nextPosition;
+    }
+}
